903e664382aff76fef63a2edfc1b916fd04d11af
nikmargit <nikola.margit@gmail.com>
2019-11-19 16:45:21 +0100

    generate rich documents

---
+++ src/components/HelloWorld/HelloWorld.js
@@ -0,0 +1,18 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function HelloWorld({ message }) {
+  return <div>Hello {message}</div>;
+}
+
+HelloWorld.propTypes = {
+  /** Message to display */
+  message: PropTypes.string
+};
+
+HelloWorld.defaultProps = {
+  /** Message to display */
+  message: "World"
+};
+
+export default HelloWorld;
---
+++ src/docs/examples/HelloWorld/ExampleHelloWorld.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+import HelloWorld from "react-lib-nikmargit/HelloWorld";
+
+/**
+ * Custom message
+ */
+export default function ExampleHelloWorld() {
+  return <HelloWorld message="this is a test"></HelloWorld>;
+}

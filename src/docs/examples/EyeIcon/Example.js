53a51d219cda13e7226f3117661096fd972b83ec
nikmargit <nikola.margit@gmail.com>
2019-11-25 14:19:16 +0100

    add first atoms

---
+++ src/docs/examples/EyeIcon/Example.js
@@ -0,0 +1,6 @@
+import React from "react";
+import EyeIcon from "react-lib-nikmargit/EyeIcon";
+
+export default function EyeIconExample() {
+  return <EyeIcon />;
+}
---
+++ src/docs/examples/Label/LabelRequired.js
@@ -0,0 +1,7 @@
+import React from "react";
+import Label from "react-lib-nikmargit/Label";
+
+/** Required label */
+export default function ExampleRequired() {
+  return <Label htmlFor="test" label="test" required />;
+}
